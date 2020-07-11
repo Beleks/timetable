589536a183fcd349cbbbc9741d8ecc1e678755f2
Beleks <53120159+Beleks@users.noreply.github.com>
2020-07-11 17:22:52 +0800

    v0.0.1

---
+++ src/components/NavbarDay.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <button
+      v-for="tab in tabs"
+      v-bind:key="tab"
+      v-bind:class="['tab-button', { active: currentTab === tab }]"
+      v-on:click="currentTab = tab"
+    >{{tab}}</button>
+    <component :is="currentTabComponent"></component>
+  </div>
+</template>
+<script>
+import tabPn from "@/components/tab-pn";
+
+export default {
+  data: () => {
+    return {
+      currentTab: "Pn",
+      tabs: ["Pn", "Vt", "Cr", "Cht", "Cb"]
+    };
+  },
+  computed: {
+    currentTabComponent: function() {
+      return "tab-" + this.currentTab.toLowerCase();
+    }
+  },
+  components: {
+      tabPn
+  }
+};
+</script>
+<style lang="scss" scoped>
+.nav {
+  //   display: grid;
+  //   grid-template-columns: minmax(100px, 1fr) 1fr 1fr;
+  //   grid-gap: 0.5em;
+}
+
+.tab-button {
+}
+
+// .block-day {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+
+//   -------------
+//     border: 1px solid black;
+//     max-width: calc(100% / 7);
+
+// }
+
+// .grid-column {
+//   display: grid;
+//   grid-template-rows: minmax(50px, 1fr) 1fr 1fr 1fr 1fr 1fr;
+// }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navbar-day></navbar-day>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavbarDay from '@/components/NavbarDay'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NavbarDay
   }
 }
 </script>
