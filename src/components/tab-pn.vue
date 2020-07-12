b3191929d81516b26a87ea8fee00456d24199f1b
Beleks <53120159+Beleks@users.noreply.github.com>
2020-07-12 19:19:43 +0800

    v0.0.3

--- src/components/tab-pn.vue
+++ src/components/tab-pn.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    Понедельник
+    <div class="block">
+      <div class="top">
+        <div class="top-par">1 пара</div>
+        <div class="top-time">09:00-10:35</div>
+      </div>
+      <div class="main-section">лек.Экологические аспекты регионального природопользования</div>
+      <div class="footer">
+        <div>Иметхенов О.А</div>
+        <div>а.8240 эбж</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
 <style lang="scss" scoped>
+.block {
+  border: 1px solid gray;
+  margin: 1em;
+  display: flex;
+  flex-direction: column;
 
+  padding: 0.5em;
+}
+.top {
+  display: flex;
+}
+.footer {
+  display: flex;
+}
 </style>
